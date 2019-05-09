42d3d47cf91f61c8e31912f339d51b30015f72c7
glebbfed <47526334+glebbfed@users.noreply.github.com>
2019-05-10 02:50:05 +0300

    Add files via upload

---
+++ NET.S.2019.Fedorcov.15/AccountSystemDemo/DAL.Interface/Interfaces/IStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL.Interface.Interfaces
+{
+    public interface IStorage
+    {
+        void AddAccount(DALAccount account);
+        void RemoveAccount(DALAccount account);
+        void DisplayStorage();
+        void SaveToFile();
+        void LoadFromFile();
+
+    }
+}
