dc5b2180f95ecb3c2902c30d7ebe344f330e2b9f
glebbfed <47526334+glebbfed@users.noreply.github.com>
2019-06-10 06:35:22 +0300

    Add files via upload

---
+++ NET.S.2019.Fedorcov.19/Gallery/Gallery/DAL/GalleryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Gallery.Models;
+
+namespace Gallery.DAL
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer<GalleryContext>
+                (new DropCreateDatabaseIfModelChanges<GalleryContext>());
+        }
+
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
